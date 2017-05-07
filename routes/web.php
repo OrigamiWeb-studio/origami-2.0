b8675a6950dfcc8dbc82ea3db41103f0aac4c1d2
age007nt <sanek.solodovnikov.94@gmail.com>
2017-05-07 14:26:48 +0200

    Change structure

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+#Homepage
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
