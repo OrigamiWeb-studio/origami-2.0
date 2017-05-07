b19d668c000cb846f1aca24cbd0cddcae46f7c1a
age007nt <sanek.solodovnikov.94@gmail.com>
2017-05-07 14:18:49 +0200

    User register and login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,68 +4,90 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+	/*
+	|--------------------------------------------------------------------------
+	| Register Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users as well as their
+	| validation and creation. By default this controller uses a trait to
+	| provide this functionality without requiring any additional code.
+	|
+	*/
+	
+	use RegistersUsers;
+	
+	/**
+	 * Where to redirect users after registration.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = '/home';
+	
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+	
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'login'    => 'required|string|max:255',
+			'email'    => 'required|string|email|max:255|unique:users',
+			'password' => 'required|string|min:6|confirmed',
+		]);
+	}
+	
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array $data
+	 * @return User
+	 */
+	protected function create($request)
+	{
+		$user = new User();
+		
+		$user->login = $request['login'];
+		$user->email = $request['email'];
+		$user->password = bcrypt($request['password']);
+		$user->last_ip = $request->ip();
+		$user->last_login = Carbon::now()->toDateTimeString();
+		$user->language_code = app()->getLocale();
+		
+		$user->save();
+		
+		return $user;
+	}
+	
+	public function register(Request $request)
+	{
+		$this->validator($request->all())->validate();
+		
+		event(new Registered($user = $this->create($request)));
+		
+		$this->guard()->login($user);
+		
+		return $this->registered($request, $user)
+			?: redirect($this->redirectPath());
+	}
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,30 +6,25 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('login');
+			$table->string('email')->unique();
+			$table->boolean('online')->default(false);
+			$table->boolean('active')->default(true);
+			$table->ipAddress('last_ip')->default('192.168.1.1');
+			$table->timestampTz('last_login');
+			$table->char('language_code', 5)->default('en');
+			$table->string('password');
+			$table->rememberToken();
+			$table->timestampsTz();
+		});
+	}
+	
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
 }
