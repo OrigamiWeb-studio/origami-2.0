d8b7159949fd070c3a7b690e609557fddd3b3bea
age007nt <sanek.solodovnikov.94@gmail.com>
2017-05-08 18:41:15 +0200

    Fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Developer;
-use App\Permission;
 use App\Project;
-use App\Role;
-use App\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
 	public function index()
 	{
 		$data = [
-			'user' => User::find(1),
-			'current_user' => User::find(2),
-			'developer' => Developer::find(1),
 			'project' => Project::find(1)
 		];
 		
