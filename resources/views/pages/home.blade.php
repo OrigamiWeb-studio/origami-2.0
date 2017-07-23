6cd6e989d8d2a3fa9b390baa25b29cd785344fd1
age007nt <sanek.solodovnikov.94@gmail.com>
2017-07-23 17:11:04 +0300

    Disable client reviews

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -36,24 +36,24 @@
 			</section>
 		@endif
 
-		@if($projects->isNotEmpty())
-			<section class="s_clients leaning_section">
-				<div class="container">
-					<h2>{{ __('Clients') }}</h2>
-					<div class="reviews-slider-wrapper">
-						<ul id="reviews-slider" class="reviews-slider list-unstyled cS-hidden">
-							@foreach($projects->where('client_review', '!=', null) as $project)
-								<li>
-									<blockquote>{{ $project->client_review }}</blockquote>
-									<strong>{{ $project->client->profile->first_name.' '.$project->client->profile->last_name }}</strong>
-									<span>{{ $project->client->profile->about }}</span>
-								</li>
-							@endforeach
-						</ul>
-					</div>
-				</div>
-			</section>
-		@endif
+		{{--@if($projects->isNotEmpty())--}}
+			{{--<section class="s_clients leaning_section">--}}
+				{{--<div class="container">--}}
+					{{--<h2>{{ __('Clients') }}</h2>--}}
+					{{--<div class="reviews-slider-wrapper">--}}
+						{{--<ul id="reviews-slider" class="reviews-slider list-unstyled cS-hidden">--}}
+							{{--@foreach($projects->where('client_review', '!=', null) as $project)--}}
+								{{--<li>--}}
+									{{--<blockquote>{{ $project->client_review }}</blockquote>--}}
+									{{--<strong>{{ $project->client->profile->first_name.' '.$project->client->profile->last_name }}</strong>--}}
+									{{--<span>{{ $project->client->profile->about }}</span>--}}
+								{{--</li>--}}
+							{{--@endforeach--}}
+						{{--</ul>--}}
+					{{--</div>--}}
+				{{--</div>--}}
+			{{--</section>--}}
+		{{--@endif--}}
 
 		<section class="s_contact leaning_section">
 			<div class="container">
