763b389d197220a1b48794adf08b591d2e0873d5
age007nt <sanek.solodovnikov.94@gmail.com>
2017-08-12 12:19:05 +0300

    Fixes

--- config/resources.php
+++ config/resources.php
@@ -1,82 +1,82 @@
 <?php
 
 return [
-	'projects' => [
-		
-		'single' => [
-			'styles'  => [
-			    'libs/lightslider/lightslider.min.css',
-                'libs/magnific-popup/magnific-popup.css',
-				'css/project-style.css'
-			],
-			'scripts' => [
-			    'libs/lightslider/lightslider.min.js',
-                'libs/magnific-popup/jquery.magnific-popup.min.js'
-			]
-		],
-		
-		'all'    => [
-			'styles'  => [
-				'css/projects-style.css'
-			],
-			'scripts' => [
-				'libs/jcf/jcf.range.js',
-				'libs/vue/vue-resource.min.js',
-				'js/filters.js'
-			]
-		],
-		
-		'edit'   => [
-			'styles'  => [
-			
-			],
-			'scripts' => [
-			
-			]
-		],
-		
-		'add'    => [
-			'styles'  => [
-			
-			],
-			'scripts' => [
-			
-			]
-		]
-	],
-	
-	
-	'home' => [
-		'styles'  => [
-			'libs/lightslider/lightslider.min.css',
-			'css/home-style.css'
-		],
-		'scripts' => [
-			'libs/vue/vue-scrollto.min.js',
-			'libs/lightslider/lightslider.min.js'
-		]
-	],
-	
-	
-	'about' => [
-		'styles'  => [
-            'libs/lightslider/lightslider.min.css',
-			'css/about-style.css'
-		],
-		'scripts' => [
-            'libs/lightslider/lightslider.min.js'
-		]
-	],
-	
-	
-	'contacts' => [
-		'styles'  => [
-			'css/contacts-style.css'
-		],
-		'scripts' => [
-			'http://maps.google.com/maps/api/js?key=AIzaSyBED1xxwdz2aeMSXBDtJwItnDn7apYZjF8'
-		]
-	]
-	
-	
+  'projects' => [
+
+    'single' => [
+      'styles' => [
+        'libs/lightslider/lightslider.min.css',
+        'libs/magnific-popup/magnific-popup.css',
+        'css/project-style.css'
+      ],
+      'scripts' => [
+        'libs/lightslider/lightslider.min.js',
+        'libs/magnific-popup/jquery.magnific-popup.min.js'
+      ]
+    ],
+
+    'all' => [
+      'styles' => [
+        'css/projects-style.css'
+      ],
+      'scripts' => [
+        'libs/jcf/jcf.range.js',
+        'libs/vue/vue-resource.min.js',
+        'js/filters.js'
+      ]
+    ],
+
+    'edit' => [
+      'styles' => [
+
+      ],
+      'scripts' => [
+
+      ]
+    ],
+
+    'add' => [
+      'styles' => [
+
+      ],
+      'scripts' => [
+
+      ]
+    ]
+  ],
+
+
+  'home' => [
+    'styles' => [
+      'libs/lightslider/lightslider.min.css',
+      'css/home-style.css'
+    ],
+    'scripts' => [
+      'libs/vue/vue-scrollto.min.js',
+      'libs/lightslider/lightslider.min.js'
+    ]
+  ],
+
+
+  'about' => [
+    'styles' => [
+      'libs/lightslider/lightslider.min.css',
+      'css/about-style.css'
+    ],
+    'scripts' => [
+      'libs/lightslider/lightslider.min.js'
+    ]
+  ],
+
+
+  'contacts' => [
+    'styles' => [
+      'css/contacts-style.css'
+    ],
+    'scripts' => [
+      'http://maps.google.com/maps/api/js?key=AIzaSyBED1xxwdz2aeMSXBDtJwItnDn7apYZjF8'
+    ]
+  ]
+
+
 ];
