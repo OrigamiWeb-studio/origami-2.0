a9a0f63cab4201f89648a29fca64cf2eafacb8e2
age007nt <sanek.solodovnikov.94@gmail.com>
2017-08-19 13:58:24 +0300

    Email requests fix

--- app/Http/Controllers/EmailRequestsController.php
+++ app/Http/Controllers/EmailRequestsController.php
@@ -2,26 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\EmailRequest;
+use App\EmailRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class EmailRequestsController extends Controller
 {
+	private function sendEmail($email_request) {
+
+		//TODO send email
+
+	}
+
 	public function saveStartProjectRequest(Request $request)
 	{
-
-//        return $request->all();
-
 		$validator = Validator::make($request->all(), [
 			'name'                 => 'required|string|between:2,255',
 			'email'                => 'required|email',
 			'description'          => 'required|string|between:4,2048',
-			'g-recaptcha-response' => 'required|captcha'
+			'g-recaptcha-response' => 'required|captcha',
 		]);
 
 		if ($validator->passes()) {
-			$sp_request = new \App\EmailRequest();
+			$sp_request = new EmailRequest();
 
 			$sp_request->name = $request['name'];
 			$sp_request->type = 'Start project';
@@ -36,26 +39,26 @@ class EmailRequestsController extends Controller
 
 			$sp_request->save();
 
+			$this->sendEmail($sp_request);
+
 			return response()->json(['success' => 'Your message has been successfully sent']);
 		}
 
 		return response()->json(['error' => $validator->errors()->all()]);
-		
-//		return redirect()->back()->with('success', 'Your message has been successfully sent');
 	}
-	
+
 	public function saveContactUsRequest(Request $request)
 	{
 		$validator = Validator::make($request->all(), [
 			'name'                 => 'required|string|between:2,255',
 			'email'                => 'required|email',
 			'description'          => 'required|string|between:4,2048',
-			'g-recaptcha-response' => 'required|captcha'
+			'g-recaptcha-response' => 'required|captcha',
 		]);
-		
+
 		if ($validator->passes()) {
-			$sp_request = new \App\EmailRequest();
-			
+			$sp_request = new EmailRequest();
+
 			$sp_request->name = $request['name'];
 			$sp_request->type = 'Contact us';
 			$sp_request->user_id = auth()->user() ? auth()->user()->id : null;
@@ -63,14 +66,14 @@ class EmailRequestsController extends Controller
 			$sp_request->email = $request['email'];
 			$sp_request->phone = isset($request['number']) ? $request['number'] : null;
 			$sp_request->message = $request['description'];
-			
+
 			$sp_request->save();
-			
+
+			$this->sendEmail($sp_request);
+
 			return response()->json(['success' => 'Your message has been successfully sent']);
 		}
-		
+
 		return response()->json(['error' => $validator->errors()->all()]);
-		
-//		return redirect()->back()->with('success', 'Your message has been successfully sent');
 	}
 }
