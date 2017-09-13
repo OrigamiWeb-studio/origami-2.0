a99f13b652d5b274954bd9ae6607029a649797cf
age007nt <sanek.solodovnikov.94@gmail.com>
2017-09-13 21:23:10 +0300

    Fixes #4: Metatags

--- resources/lang/pl/static.php
+++ resources/lang/pl/static.php
@@ -2,7 +2,11 @@
 
 return [
 
+	#Metatags
+	'meta_description' => 'Origami Web Studio is professional developers team which developing high-quality web-sites any level complexity and for all types of devices',
+	'meta_keywords'    => 'Website development, site, web studio, development, order',
+
 	#About us page
-	'about_us_text' => 'Origami Team is professional developers team which major on developing, promoting and supporting websites. Foundation-stone of our work is individual approach and high-quality development based on modern technologies. To achieve maximum result we divide our projects on steps which doing our experts in their areas. Each step is being discussed in details and being confirmed with clients. Therefore we are doing only necessary things for customer.',
+	'about_us_text'    => 'Origami Team is professional developers team which major on developing, promoting and supporting websites. Foundation-stone of our work is individual approach and high-quality development based on modern technologies. To achieve maximum result we divide our projects on steps which doing our experts in their areas. Each step is being discussed in details and being confirmed with clients. Therefore we are doing only necessary things for customer.',
 
 ];
--- resources/views/head/meta.blade.php
+++ resources/views/head/meta.blade.php
@@ -2,3 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+
+<meta name="description" content="{{ __('static.meta_description') }}">
+<meta name="keywords" content="{{ __('static.meta_keywords') }}">
+
+{{--TODO #1: зробити можливість динамічно додавати keywords--}}
