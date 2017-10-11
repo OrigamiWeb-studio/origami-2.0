0e8ccf35ddd979d1d690bdbedd0c834497fe3499
age007nt <sanek.solodovnikov.94@gmail.com>
2017-10-11 17:41:15 +0200

    Fix adding project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -284,6 +284,7 @@ class ProjectsController extends Controller
 		$project->client_review = $request['client_review'];
 		$project->description = $request['description'];
 		$project->short_description = $request['short_description'];
+		$project->closed_at = Carbon::createFromFormat('d.m.Y H:i:s', $request['closed_at'] . ' 00:00:00');
 
 		if (Input::hasFile('cover')) {
 			if ($project->cover)
