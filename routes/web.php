add3dc23f0456a465fe72b46bb532b65cd972b6e
age007nt <sanek.solodovnikov.94@gmail.com>
2017-10-20 09:18:47 +0200

    Using project slug instead of project id (fix #3)

--- routes/web.php
+++ routes/web.php
@@ -44,27 +44,23 @@ Route::post('/projects/add', 'ProjectsController@addProject')
 	->middleware(['role:owner']);
 
 Route::get('/projects/{slug}/edit', 'ProjectsController@editProjectView')
-	->where('slug', '[a-z0-9_-]+')
+	->where('slug', '[a-zA-Z0-9_-]+')
 	->name('project-edit')
 	->middleware(['role:owner']);
 
 Route::post('/projects/{slug}/edit', 'ProjectsController@editProject')
-	->where('slug', '[a-z0-9_-]+')
+	->where('slug', '[a-zA-Z0-9_-]+')
 	->name('project-edit-submit')
 	->middleware(['role:owner']);
 
 Route::get('/projects/{slug}/delete', 'ProjectsController@deleteProject')
-	->where('slug', '[a-z0-9_-]+')
+	->where('slug', '[a-zA-Z0-9_-]+')
 	->name('project-delete-submit')
 	->middleware(['role:owner']);
 
-//Route::get('/projects/{id}', 'ProjectsController@singleProject')
-//	->name('project')
-//	->where('id', '[0-9]+');
-
 Route::get('/projects/{slug}', 'ProjectsController@singleProject')
 	->name('project')
-	->where('slug', '[a-z0-9_-]+');
+	->where('slug', '[a-zA-Z0-9_-]+');
 
 Route::get('/projects/{project_id}/screenshots/{id}/delete', 'ProjectsController@deleteScreenshot')
 	->name('project-screenshot-delete-submit')
@@ -82,11 +78,11 @@ Route::get('/projects/{project_id}/screenshots', 'ProjectsController@projectScre
 #
 Route::get('/projects/{project_slug}/tickets', 'TicketsController@projectTickets')
 	->name('project-tickets')
-	->where('project_slug', '[a-z0-9_-]+');
+	->where('project_slug', '[a-zA-Z0-9_-]+');
 
 Route::get('/projects/{project_slug}/tickets/{id}', 'TicketsController@singleTicket')
 	->name('ticket')
-	->where('project_slug', '[a-z0-9_-]+')
+	->where('project_slug', '[a-zA-Z0-9_-]+')
 	->where('id', '[0-9]+');
 #-----------------------------------------------------------------------------------
 #About
